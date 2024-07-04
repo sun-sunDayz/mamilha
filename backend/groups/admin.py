fba0e3d9c1235e5fbd5bbf77dd81a88a9820e901
hacker <hacker@hackerui-MacBookPro.local>
2024-07-05 07:14:50 +0900

    random User Create

--- backend/groups/admin.py
+++ backend/groups/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Group, Currency, Group_category
 
 # Register your models here.
+admin.site.register(Group)
+admin.site.register(Group_category)
+admin.site.register(Currency)
