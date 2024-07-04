acb01531d3b4c245e6d3e1d0b575d5128c16c79e
hacker <hacker@hackerui-MacBookPro.local>
2024-07-05 07:14:50 +0900

    random User Create

--- backend/finances/admin.py
+++ backend/finances/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Finance, FinanceCategory, FinanceType, PayMethod, SplitMethod
 # Register your models here.
+
+admin.site.register(Finance)
+admin.site.register(FinanceCategory)
+admin.site.register(FinanceType)
+admin.site.register(PayMethod)
+admin.site.register(SplitMethod)
