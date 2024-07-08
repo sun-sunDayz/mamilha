423c04b7149e77cb7187e2e0ea23a9755ade1ca6
Heeky <heekyjung@gmail.com>
2024-07-09 03:25:29 +0900

    feature: member, split_method, pay_method

--- backend/finances/models.py
+++ backend/finances/models.py
@@ -3,33 +3,45 @@ from django.utils import timezone
 from groups.models import Group
 from django.contrib.auth import get_user_model
 
+
 class FinanceType(models.Model):
     name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
-    
+
+
 class FinanceCategory(models.Model):
     name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
+
 
 class PayMethod(models.Model):
     name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
+
 
 class SplitMethod(models.Model):
     name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
 
+
 User = get_user_model()
 # Create your models here.
+
+
 class Finance(models.Model):
     payer = models.ForeignKey(User, on_delete=models.CASCADE)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     finance_type = models.ForeignKey(FinanceType, on_delete=models.CASCADE)
-    finance_category = models.ForeignKey(FinanceCategory, on_delete=models.CASCADE)
+    finance_category = models.ForeignKey(
+        FinanceCategory, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField(default=timezone.now)
     description = models.TextField(null=True, blank=True)
@@ -44,3 +56,11 @@ class Finance(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class PayMethod(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class SplitMethod(models.Model):
+    name = models.CharField(max_length=20)
