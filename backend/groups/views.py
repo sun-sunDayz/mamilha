ce519554f709229d6f4f995961497aa2a6b58c65
Heeky <heekyjung@gmail.com>
2024-09-04 22:09:42 +0900

    프론트: 지출생성/편집 추가 / 백: 그룹 멤버&그레이드 API 재추가

--- backend/groups/views.py
+++ backend/groups/views.py
@@ -264,3 +264,162 @@ class GroupSplit(APIView):
                 })
 
         return Response(data, status=status.HTTP_200_OK)
+
+class MemberAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, group_pk):
+        group = get_object_or_404(Group, pk=group_pk)
+
+        member_list = group.Member_group.all()
+
+        members = []
+        for i in member_list:
+            members.append({
+                "id": i.pk,
+                "name": i.name,
+                "grades": i.grades.name,
+                "active": i.active,
+            })
+
+        return Response(members,
+                        status=status.HTTP_200_OK)
+
+    # 멤버 생성
+    def post(self, request, group_pk):
+        group = get_object_or_404(Group, pk=group_pk)
+        user = request.user
+
+        # 권한 체크
+        if not self.is_allowed_to_create(user):
+            return Response({'error': "멤버 생성 권한이 없습니다."}, status=status.HTTP_403_FORBIDDEN)
+
+        data = request.data
+        id = data.get('id')
+        name = data.get('name')
+        grades = data.get('grades')
+        active = data.get('active', True)
+
+        # 이름이 빈값일 경우 Error 처리
+        if not name:
+            return Response({'error': "이름을 입력해주세요."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # 등급 없으면 Error 처리
+        if not grades:
+            return Response({'error': "등급을 선택해주세요."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # 같은 이름 멤버 Error 처리
+        if Member.objects.filter(group=group, name=name).exists():
+            return Response({'error': "같은 이름의 멤버가 이미 존재합니다."}, status=status.HTTP_400_BAD_REQUEST)
+
+        new_member = Member.objects.create(
+            name=name,
+            grades=grades,
+            group=group,
+            active=active,
+        )
+
+        return Response({
+            "message": "멤버 추가가 완료되었습니다.",
+            "member_pk": new_member.pk,
+            "name": new_member.name,
+            "grades": new_member.grades,
+            "active": new_member.active,
+        }, status=status.HTTP_201_CREATED)
+
+    # 권한 체크 함수
+    def is_allowed_to_create(self, user):
+        try:
+            user_grade = Member.objects.get(user=user).grades.name
+            return user_grade in ["admin", "exec"]  # 수정 필요
+        except Member.DoesNotExist:
+            return False
+
+
+class MemberDetail(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, group_pk, member_pk):
+        member = get_object_or_404(Member, pk=member_pk, group__pk=group_pk)
+
+        return Response({
+            "id": member.pk,
+            "name": member.name,
+            "grades": member.grades.name,
+            "active": member.active,
+        }, status=status.HTTP_200_OK)
+
+    def put(self, request, group_pk, member_pk):
+        member = get_object_or_404(Member, pk=member_pk, group__pk=group_pk)
+        user = request.user
+
+        # 권한 체크
+        if not self.is_allowed_to_edit(user):
+            return Response({'error': "멤버 수정 권한이 없습니다."}, status=status.HTTP_403_FORBIDDEN)
+
+        data = request.data
+        
+        new_id = data.get('id', member.pk)
+        new_name = data.get('name', member.name)
+        new_grades = Grades.objects.get(
+            name=data.get('grades', member.grades.name))
+        new_active = data.get('active', member.active)
+
+        # 이름이 빈값일 경우 Error 처리
+        if not new_name:
+            return Response({'error': "멤버 이름이 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # 같은 이름 멤버 Error 처리, 단 기존의 이름과 같은 경우는 제외
+        if new_name != member.name and Member.objects.filter(group__pk=group_pk, name=new_name).exists():
+            return Response({'error': "같은 이름의 멤버가 이미 존재합니다."}, status=status.HTTP_400_BAD_REQUEST)
+
+        member.name = new_name
+        member.grades = new_grades
+        member.active = new_active
+
+        member.save()
+
+        return Response({
+            "name": member.name,
+            "grades": member.grades.name,
+            "active": member.active,
+        }, status=status.HTTP_200_OK)
+
+    def delete(self, request, group_pk, member_pk):
+        member = get_object_or_404(Member, pk=member_pk, group__pk=group_pk)
+        user = request.user
+
+        # 권한 체크
+        if not self.is_allowed_to_edit(user):
+            return Response({'error': "멤버 삭제 권한이 없습니다."}, status=status.HTTP_403_FORBIDDEN)
+
+        member.deleted = True
+        member.save()
+        return Response({"message: 멤버 삭제가 완료되었습니다."}, status=status.HTTP_200_OK)
+
+    # 권한 체크 함수
+    def is_allowed_to_edit(self, user):
+        try:
+            user_grade = Member.objects.get(user=user).grades.name
+            return user_grade in ["admin", "exec"]  # 수정 필요
+        except Member.DoesNotExist:
+            return False
+
+
+class MemberGrades(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        grades_list = Grades.objects.all()
+
+        grades = []
+        for grade in grades_list:
+            grades.append({
+                "id": grade.id,
+                "name": grade.name,
+                "admin": grade.admin,
+                "edit": grade.edit,
+                "view": grade.view,
+            })
+
+        return Response(grades, status=status.HTTP_200_OK)
---
+++ frontend/src/screens/CreateFinance.js
@@ -0,0 +1,60 @@
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {useNavigation} from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import FinanceForm from '../components/FinanceForm';
+
+const CreateFinance = () => {
+  const navigation = useNavigation(); // 네비게이션 객체 가져오기
+
+  const handleGoBack = () => {
+    navigation.goBack(); // 뒤로가기 액션
+  };
+
+  // 지출 생성
+  const handleCreate = formData => {
+    navigation.goBack(); // 생성 후 뒤로가기
+  };
+
+  const handleFormSubmit = async data => {
+    // FinanceForm에서 호출되며, 이미 handleSubmit에서 데이터를 서버로 전송하는 코드가 포함되어 있음
+    navigation.goBack(); // 생성 후 뒤로가기
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon name="close" size={40} color="#616161" />
+        </TouchableOpacity>
+        <Text style={styles.headerText}>지출 등록</Text>
+        {/* 빈 공간 */}
+        <View style={{width: 40}}></View>
+      </View>
+      <View style={styles.content}>
+        <FinanceForm onSubmit={handleFormSubmit} buttonLabel="생성하기" />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default CreateFinance;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f1f1f9',
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 41,
+    paddingHorizontal: 20,
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
