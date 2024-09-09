d7ca5307523b251c018c5e0733c6a87d7e15f04a
sang-il <lsi9393@naver.com>
2024-09-09 13:22:51 +0900

    feat login * 로그인 되었을 때 main으로 네비게이션

--- frontend/src/screens/Login.js
+++ frontend/src/screens/Login.js
@@ -7,19 +7,18 @@ import {
   Alert,
   TouchableOpacity,
 } from 'react-native';
-import apiClient from '../services/apiClient';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { login } from '../api/Accounts';
 
-const Login = () => {
+const Login = ({navigation}) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('아이디 또는 비밀번호를 다시 확인하세요'); // 에러 메시지 상태
+  const [error, setError] = useState(''); 
 
   const handleLogin = async e => {
     const result = await login(username, password)
     if(result) {
-      setError('로그인이 완료되었습니다.')
+      setError('')
+      navigation.navigate('Main');
     } else {
       setError('로그인에 실패했습니다.')
     }
