cc67da0998d163f67f7fdab5ebf24b7747eac225
김병민 <gimbyeongmin@byeong.local>
2024-09-10 06:45:02 +0900

    main screend에서 hadle 수정

--- backend/groups/views.py
+++ backend/groups/views.py
@@ -49,7 +49,7 @@ class GroupAPIView(APIView):
             members = Member.objects.filter(group_id = i.group_id)
             # members[0]은 언제나 관리자
             groups.append({
-                "id" :  i.id,
+                "id" :  i.group_id,
                 "name": i.group.name,
                 "category" : i.group.category.name,
                 "currency": i.group.currency.currency,
