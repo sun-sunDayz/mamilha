efdc207aa6068c24e2eca5bc84a1ef43c4e0d660
LeeJS9856 <wnstj1057@naver.com>
2024-11-14 13:12:47 +0900

    design : 그룹수정 UI 수정

--- frontend/src/components/GroupForm.js
+++ frontend/src/components/GroupForm.js
@@ -226,19 +226,19 @@ const GroupForm = ({ group_pk, initialData = {}, screenName }) => {
                 </ScrollView>
             </View>
             {screenName == 'CreateGroup' ?
-                <TouchableOpacity onPress={handleSubmit} style={styles.CreateGroupButton} >
-                    <Text style={styles.CreateGroupButtonText}>생성하기</Text>
-                </TouchableOpacity>
+                    <TouchableOpacity onPress={handleSubmit} style={styles.CreateGroupButton} >
+                        <Text style={styles.CreateGroupButtonText}>생성하기</Text>
+                    </TouchableOpacity>
                 :
-                <TouchableOpacity onPress={() => setIsUpdateModalOpen(true)}
-                    style={styles.CreateGroupButton}
-                    onLayout={(event) => {
-                        const { width } = event.nativeEvent.layout;
-                        setModalWidth(width);
-                    }}>
-                    <Text style={styles.CreateGroupButtonText}>수정하기</Text>
-                </TouchableOpacity>}
-
+                    <TouchableOpacity onPress={() => setIsUpdateModalOpen(true)}
+                        style={styles.CreateGroupButton}
+                        onLayout={(event) => {
+                            const { width } = event.nativeEvent.layout;
+                            setModalWidth(width);
+                        }}>
+                        <Text style={styles.CreateGroupButtonText}>수정하기</Text>
+                    </TouchableOpacity>
+            }
             {isUpdateModalOpen && (
                 <Modal
                     transparent={true}
@@ -287,8 +287,7 @@ const styles = StyleSheet.create({
         borderRadius: 8,
     },
     ScrollViewContent: {
-        padding: 10,
-        paddingBottom: 150,
+        paddingBottom: 200,
     },
     formRow: {
         flexDirection: 'column',
@@ -351,17 +350,17 @@ const styles = StyleSheet.create({
     CreateGroupButton: {
         position: 'absolute',
         alignSelf: 'center',
-        width: '80%',
         top: 500,
-        padding: 13,
+        width: '85%',
+        padding: 15,
+        alignItems: 'center',
         backgroundColor: '#5DAF6A',
         borderRadius: 10,
-        alignItems: 'center',
     },
     CreateGroupButtonText: {
         color: '#ffffff',
         fontSize: 16,
-        fontWeight: '700',
+        fontWeight: '800',
     },
     // 활성화버튼
     activeButton: {
